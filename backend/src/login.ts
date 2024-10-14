1be3ca619c0ceb0a6f0a9c565cef214a253d9b68
Henrick <z5533795@ad.unsw.edu.au>
2024-10-14 11:52:09 +1100

    js to ts

---
+++ backend/src/login.ts
@@ -0,0 +1,11 @@
+/**
+ * Login a user and return a token
+ * @param {string} username 
+ * @param {string} password 
+ * @returns {string} token
+ */
+export function login(username: string, password: string): string {
+    // TODO: implement login
+    // For now, return a dummy token
+    return "dummy_token";
+}
