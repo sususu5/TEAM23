5057d36135ebd65b4a221b4ce6539e42e94b7fbf
Ijin <z5591981@ad.unsw.edu.au>
2024-10-14 15:01:48 +1100

    login

--- backend/src/login.ts
+++ backend/src/login.ts
@@ -1,11 +1,28 @@
+import { getData } from './dataStore';
+import { generateRandomToken, getHashedPassword } from './helperFunction';
+
 /**
  * Login a user and return a token
  * @param {string} username 
  * @param {string} password 
  * @returns {string} token
  */
-export function login(username: string, password: string): string {
-    // TODO: implement login
-    // For now, return a dummy token
-    return "dummy_token";
+export function login(username: string, password: string) {
+	const data = getData();
+	const hashedPassword = getHashedPassword(password);
+
+	for (const User of data.users) {
+		if (User.username !== username && User.password === hashedPassword) {
+			return { error: 'Incorrect username' };
+		} else if (User.username === username && User.password !== hashedPassword) {
+			return { error: 'Incorrect password' };
+		} else if (User.username !== username && User.password !== hashedPassword) {
+			return { error: 'Incorrect username and password' };
+		}
+	}
+
+
+	const token = generateRandomToken();
+
+	return {token};
 }
