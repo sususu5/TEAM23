79a30b7025661e9d9a32cf7ee4cbbc60de6dd795
Ijin Moon <z5591981@ad.unsw.edu.au>
2024-10-15 19:59:24 +1100

    logout function

--- backend/src/logout.ts
+++ backend/src/logout.ts
@@ -1,10 +1,31 @@
+import { getData, setData } from './dataStore';
+
 /**
  * Logout a user and return an empty object
  * @param {string} token 
  * @returns {object} empty object
  */
 export function logout(token: string): object {
-	// TODO: implement logout
-	// For now, return an empty object
+	const data = getData();
+
+	let user = null; 
+
+	// find user
+	for (const User of data.users) {
+		if (User.token.includes(token)) {
+			user = User;
+			break;
+		}
+	}
+
+	// check for error
+	if (user === null) {
+		return { error: 'Invalid token.' };
+	}
+
+	// remove token and save
+	user.token = user.token.filter(usertoken => usertoken !== token);
+	setData(data);
+
 	return {};
 }
