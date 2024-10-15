d2610cb1f784051f070d6a174d4fa9e4834a7cfd
Henrick <z5533795@ad.unsw.edu.au>
2024-10-15 21:59:18 +1100

    user related functions, add clear in tests

--- backend/src/updateUserDetails.ts
+++ backend/src/updateUserDetails.ts
@@ -1,4 +1,5 @@
 import { setData, getData } from './dataStore';
+import { containsValidName } from './helperFunction';
 /**
  * Update the user's details
  * @param token 
@@ -9,6 +10,14 @@ import { setData, getData } from './dataStore';
 export function updateUserDetails(token: string, username: string, avatar: string) {
     const data = getData();
     const user = data.users.find(u => u.token.includes(token));
+    if (!user) {
+        return { error: 'Invalid token' };
+    }
+    if (!containsValidName(username)) {
+        return {
+          error: 'username contains restricted characters.'
+        };
+    }
     user.username = username;
     user.avatar = avatar;
 
