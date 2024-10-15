76f94717cbb29510c13f9fd8a680a9777f916b37
Ijin Moon <z5591981@ad.unsw.edu.au>
2024-10-15 22:39:39 +1100

    added clear to tests

--- backend/src/logout.test.ts
+++ backend/src/logout.test.ts
@@ -1,14 +1,14 @@
 import { login } from './login';
 import { logout } from './logout';
 import { register } from './register';
-//import { clear } from "../helperFunction";
+import { clear } from "../helperFunction";
 let user: any;
 let loginreturn: any;
 
 const DEFAULT_THUMBNAIL_URL = 'https://mrvian.com/wp-content/uploads/2023/03/unsw-logo.png';
 
 beforeEach(() => {
-	//clear();
+	clear();
   user = register('Mark', 'Comp1531YAY', DEFAULT_THUMBNAIL_URL);
   loginreturn = login('Mark', 'Comp1531YAY');
 });
