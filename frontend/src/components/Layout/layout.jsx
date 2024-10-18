1c275e157b19eb90237f784494f1acdecd0cbd65
Jade Hall <z5588239@ad.unsw.edu.au>
2024-10-19 02:37:14 +1100

    moved files into components folder

---
+++ frontend/src/components/Layout/layout.jsx
@@ -0,0 +1,42 @@
+import { Outlet,  Link } from "react-router-dom";
+import { useState } from 'react'
+import MyNoteButton from '../MyNoteButton/myNoteButton';
+import './layout.css'
+
+function Layout() {
+  const [user] = useState(null);
+  return (
+    <>
+      <nav className="navbar">
+        <h1>StudyShare</h1>
+
+        <div className="navbuttons">
+          <a href="/">
+            <button className="button">Home</button>
+          </a>
+          {user ? (
+          <>
+            <MyNoteButton />
+            </>
+          ) : (
+            <></>
+          )}
+        </div>
+        
+        <div className="login">
+          {user ? (
+            <>
+              <button>Logout</button>
+            </>
+          ) : (
+            <a href="/login">
+              <button>Login</button>
+            </a>
+          )}
+        </div>  
+      </nav>
+      <Outlet />
+    </>
+  )
+};
+export default Layout;
